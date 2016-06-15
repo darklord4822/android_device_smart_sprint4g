428139f53413ab3b196305708e2dc2836c306fb9
Ubuntu <darklord4822@SRT.SRT-50t7o38d.b8.internal.cloudapp.net>
2016-06-15 22:16:45 +0000

    first commit

---
+++ cm.mk
@@ -0,0 +1,21 @@
+# Release name
+PRODUCT_RELEASE_NAME := sprint4g
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/smart/sprint4g/device_sprint4g.mk)
+
+# Configure dalvik heap
+$(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
+
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sprint4g
+PRODUCT_NAME := cm_sprint4g
+PRODUCT_BRAND := sprint4g
+PRODUCT_MODEL := sprint4g
+PRODUCT_MANUFACTURER := sprint4g
