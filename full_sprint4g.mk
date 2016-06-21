ce05d68c2cddd89c9b19c225f96330c761725f30
Ubuntu <darklord4822@SRT.SRT5801.b7.internal.cloudapp.net>
2016-06-21 10:11:26 +0000

    first commit

---
+++ full_sprint4g.mk
@@ -0,0 +1,28 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/smart/sprint4g/device.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := Smart Sprint 4G
+
+EXTENDED_FONT_FOOTPRINT := true
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.timezone=Europe/Moscow
+
+PRODUCT_DEVICE := sprint4g
+PRODUCT_NAME := full_sprint4g
+PRODUCT_BRAND := sprint4g
+PRODUCT_MODEL := Smart Sprint 4G
+PRODUCT_MANUFACTURER := sprint4g
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Boot animation
+TARGET_SCREEN_HEIGHT      := 854
+TARGET_SCREEN_WIDTH       := 480
+TARGET_BOOTANIMATION_NAME := 480
+
+PRODUCT_DEFAULT_LANGUAGE := ru
+PRODUCT_DEFAULT_REGION   := RU
---
+++ slim.mk
@@ -0,0 +1,26 @@
+# Release name
+PRODUCT_RELEASE_NAME := sprint4g
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/smart/sprint4g/device_sprint4g.mk)
+
+# Configure dalvik heap
+$(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
+
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sprint4g
+PRODUCT_NAME := slim_sprint4g
+PRODUCT_BRAND := smart
+PRODUCT_MODEL := sprint4g
+PRODUCT_MANUFACTURER := smart
+
+PRODUCT_DEFAULT_LANGUAGE := ru
+PRODUCT_DEFAULT_REGION   := RU
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.timezone=Europe/Moscow
